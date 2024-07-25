2ce8aeaf229a5b072dce24c4475dc73c03c7430b
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2024-07-25 20:59:52 +0900

    Style: Auto Lint 추가

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "harmony",
+  products: [
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", from: "1.0.5"),
+  ],
+  targets: [
+  ])
